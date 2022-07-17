669cbc9702a2d1f3f98d17e2f42188201259152f
Eric Knapp <eric.knapp011@gmail.com>
2022-07-17 16:22:02 -0400

    updates to tvShows

--- lab8/data/tvShows.js
+++ lab8/data/tvShows.js
@@ -3,8 +3,8 @@ const axios = require("axios");
 async function getTv() {
   const { data } = await axios.get(
     // biggest loser
-    // "http://api.tvmaze.com/search/shows?q=The Biggest Loser"  
-    "http://api.tvmaze.com/shows?" //all data
+    // "http://api.tvmaze.com/search/shows?q=The Biggest Loser"
+    "http://api.tvmaze.com/shows?" // all data from api.tvmaze.com/shows
   );
   return data;
 }
