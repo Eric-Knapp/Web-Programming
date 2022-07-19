8b6a146f4ad3ce639455902afb1caea3e75860d7
Eric Knapp <eric.knapp011@gmail.com>
2022-07-18 23:57:20 -0400

    updates

--- lab8/routes/pages.js
+++ lab8/routes/pages.js
@@ -2,22 +2,24 @@ const express = require("express");
 const axios = require("axios");
 const router = express.Router();
 
+// main page to showFinder
 router.get("/", async (req, res) => {
   try {
-    res.render("pages/showFinder", { title: "Show Finder" });
+    res.render("webpages/showFinder", { title: "Show Finder" });
   } catch (e) {
     res.status(400).json({ error: "show finder not found" });
   }
 });
 
+//search page
 router.post("/search", async (req, res) => {
   if (!req.body) {
-    res.status(400).render("pages/errorFile");
+    res.status(400).render("webpages/errorFile");
     return;
   }
   const { searchTerm } = req.body;
   if (searchTerm.trim().length === 0) {
-    res.status(400).render("pages/errorFile");
+    res.status(400).render("webpages/errorFile");
     return;
   }
   try {
@@ -25,7 +27,7 @@ router.post("/search", async (req, res) => {
       `http://api.tvmaze.com/search/shows?q=${searchTerm}`
     );
     data.slice(0, 20);
-    res.render("pages/showsFound", {
+    res.render("webpages/showsFound", {
       title: "Shows Found",
       showAmount: data.length !== 0,
       shows: data,
@@ -36,21 +38,22 @@ router.post("/search", async (req, res) => {
   }
 });
 
+//get show by id
 router.get("/shows/:id", async (req, res) => {
   try {
     const { id } = req.params;
 
     if (isNaN(id)) {
-      res.status(404).render("pages/errorFile");
+      res.status(404).render("webpages/errorFile");
     }
     if (!Number.isInteger(parseFloat(id)) || parseFloat(id) < 0) {
-      res.status(404).render("pages/errorFile");
+      res.status(404).render("webpages/errorFile");
     }
-
+    //getting tv show data
     const { data } = await axios.get(`http://api.tvmaze.com/shows/${id}`);
     data.summary = data.summary.replace(/(<([^>]+)>)/gi, "");
 
-    res.render("pages/showFound", { title: data.name, show: data });
+    res.render("webpages/showFound", { title: data.name, show: data });
   } catch (e) {
     res.sendStatus(500);
   }
