04dce0a1485e2d998b431fd252f30c14479b68bb
Eric Knapp <eric.knapp011@gmail.com>
2022-08-01 21:46:10 -0400

    08/22 updates

--- lab10/routes/pages.js
+++ lab10/routes/pages.js
@@ -3,65 +3,72 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const users = require("../data/users");
 
+//renders error if not authenticated
 function Authenticate(req, res, next) {
-  if (req.session.user) return next();
-  else res.status(403).render("error", { title: "Invalid Entry" }); //changed invalid to error
+  if (req.session.person) return next();
+  else res.status(403).render("error", { title: "Invalid Entry" });
 }
 
+//renders login screen
 router.get("/", function (req, res) {
-  if (req.session.user) {
+  if (req.session.person) {
     res.redirect("/private");
   } else {
     res.render("login", { title: "Login" });
   }
 });
 
+//description
 router.get("/private", Authenticate, (req, res) => {
-  res.render("description", req.session.user);
+  res.render("description", req.session.person);
 });
 
+//logout screen
 router.get("/logout", async (req, res) => {
   req.session.destroy();
   res.render("logout", { message: "Logged out successfully!" });
 });
 
 router.post("/login", async (req, res) => {
-  let inval = false;
+  let err = false;
   if (req.body.username && req.body.password) {
-    for (let user in users) {
-      if (users[user].username == req.body.username) {
-        let match = await bcrypt.compare(
+    //loop through users using bcryot to check username and password
+    for (let person in users) {
+      if (users[person].username == req.body.username) {
+        let pair = await bcrypt.compare(
           req.body.password,
-          users[user].hashedPassword
+          users[person].hashedPassword
         );
 
-        if (match) {
-          req.session.user = {
-            username: users[user].username,
-            password: users[user].hashedPassword,
-            _id: users[user]._id,
-            firstName: users[user].firstName,
-            lastName: users[user].lastName,
-            profession: users[user].profession,
-            bio: users[user].bio,
-          };
+        //if username/password non existent/false, then below gets thrown
+        if (err) {
+          res.status(401).render("login", {
+            title: "Username/Password Invalid",
+            error: "Username/Password Invalid",
+          });
+        }
 
+        //if correct info - descriptive info below
+        if (pair) {
+          req.session.person = {
+            username: users[person].username,
+            password: users[person].hashedPassword,
+            _id: users[person]._id,
+            firstName: users[person].firstName,
+            lastName: users[person].lastName,
+            profession: users[person].profession,
+            bio: users[person].bio,
+          };
           res.redirect("/private");
         } else {
-          inval = true;
+          err = true;
         }
       } else {
-        inval = true;
+        err = true;
       }
     }
   } else {
-    inval = true;
-  }
-  if (inval) {
-    res.status(401).render("login", {
-      title: "Username/Password Invalid",
-      error: "Username/Password Invalid",
-    });
+    err = true;
   }
 });
 
